4ca5ea167d0fd32c2cf411fea773d13734f016e3
Araf Hossain <arafh13@gmail.com>
2019-10-11 12:49:01 -0400

    Created axios.get request to cards API

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import './Deck.css'
+import axios from 'axios'
 
 class Deck extends Component {
   constructor(props){
     super(props)
-    this.state = {};
+    this.state = {deck_id: ''};
+  }
+  async componentDidMount(){
+    await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/').then(response => {
+      this.setState({ deck_id: response.data.deck_id })
+    })
   }
   render(){
     return (
       <div className="Deck">
         <h1 className="Header">♤ Flip a card! ♤</h1>
         <h3>Deck here</h3>  
+
       </div>
     )
   }
