5cde12e15159476c429864fd1349e7e988256c26
Araf Hossain <arafh13@gmail.com>
2019-10-13 19:45:29 -0400

    Different transform values and background

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import './Card.css'
 class Card extends Component {
   constructor(props) {
     super(props);
-    let angle = Math.random() * 90 - 45;
-    let xPos = Math.random() * 40 - 20;
-    let yPos = Math.random() * 40 - 20;
+    let angle = Math.random() * 90 - 30;
+    let xPos = Math.random() * 40 - 30;
+    let yPos = Math.random() * 40 - 30;
     this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
   }
   render(){
